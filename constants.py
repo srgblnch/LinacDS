fc40045d44cd5ffa9da4e72977909ccdb981fa74
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2015-09-01 14:30:28 +0000

    about the 'too far' feature, inhibit the check when the setpoint is too close to 0.
    git-svn-id: https://svn.code.sf.net/p/tango-ds/code/DeviceClasses/AcceleratorComponents/LinacAlba/trunk@20568 3b39871e-fe4b-0410-b5db-e82997f68959

---
+++ constants.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# constants.py
+# This file is part of tango-ds (http://sourceforge.net/projects/tango-ds/)
+#
+# tango-ds is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tango-ds is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tango-ds.  If not, see <http://www.gnu.org/licenses/>.
+
+MAJOR_VERSION = 2
+MINOR_VERSION = 2
+BUILD_VERSION = 8
+
+#---- attributes keys
+READADDR = 'read_addr'
+READBIT = 'read_bit'
+READVALUE = 'read_value'
+READTIME = 'read_t'
+WRITEADDR = 'write_addr'
+WRITEBIT = 'write_bit'
+WRITEVALUE = 'write_value'
+TYPE = 'type'
+FORMULA = 'formula'
+LASTEVENTQUALITY = 'lastEventQuality'
+ENABLE = 'Enable'
+READBACK = 'readbackAttr'
+SETPOINT = 'setpointAttr'
+SWITCH = 'SwitchAttr'
+
+#---- internal logic attrs
+LOGIC = 'logic'
+OPERATOR = 'operator'
+INVERTED = 'inverted'
+
+#---- resets
+ISRESET = 'isRst'
+RESETTIME = 'rst_t'
+RESETACTIVE = 'activeRst_t'
+
+#---- qualities and events
+QUALITIES = 'qualities'
+EVENTS = 'events'
+CHANGING = 'changing'
+WARNING = 'warning'
+ALARM = 'alarm'
+ABSOLUTE = 'abs'
+RELATIVE = 'rel'
+ABOVE = 'Above'
+BELOW = 'Below'
+UNDER = 'Under'
+MEANINGS = 'meanings'
+
+#---- periods
+EVENT_THREAD_PERIOD = 0.320#s
+ACTIVE_RESET_T = EVENT_THREAD_PERIOD*3#s
+PLC_MIN_UPDATE_PERIOD = EVENT_THREAD_PERIOD/2#s
+PLC_MAX_UPDATE_PERIOD = EVENT_THREAD_PERIOD*2#s
+PLC_STEP_UPDATE_PERIOD = EVENT_THREAD_PERIOD/10#s
+EXPECTED_UPDATE_TIME = PLC_MAX_UPDATE_PERIOD#s or less
+
+#---- ramping
+RAMP = 'Ramp'
+RAMPDEST = 'ramp_dest'
+STEP = 'Step'
+STEPTIME = 'Steptime'
+THRESHOLD = 'Threshold'
+RAMPENABLE = 'rampEnable'
+ASCENDING = 'Ascending'
+DESCENDING = 'Descending'
+SWITCHDESCRIPTOR = 'switchDescriptor'
+SWITCHDEST = 'switch_dest'
+WHENON = 'whenOn'
+SWITCHONSLEEP = 1#s
+WHENOFF = 'whenOff'
+ATTR2RAMP = 'attr2ramp'
+FROM = 'from'
+TO = 'to'
+
+#---- Historic buffer
+BASESET = 'baseSet'
+HISTORYLENGTH = 20
+
+#---- conditional stopper
+AUTOSTOP = 'AutoStop'
+INTEGRATIONTIME = 'IntegrationTime'
+MEAN = 'Mean'
+STD = 'Std'
+TRIGGERED = 'Triggered'
+
+#---- Readback far from setpoint
+from PyTango import AttrQuality
+WARNING_REL_DISTANCE = 0.1
---
+++ plck.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+# plck.py
+# This file is part of tango-ds (http://sourceforge.net/projects/tango-ds/)
+#
+# tango-ds is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tango-ds is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tango-ds.  If not, see <http://www.gnu.org/licenses/>.
+
+'''Schema of the attributes:
+   Attr(name,             :Name of the dynamic attribute
+        T,                :Tango type of the attribute
+        read_addr=None,   :PLC register address for read operation
+        write_addr=None,  :PLC register address for write operation
+        isa=None,         :??TODO
+        **kwargs          :
+
+   AttrBit(name,            :Name of the dynamic attribute
+           read_addr=None,  :PLC register address for read operation
+           bitno=0,         :Bit of the read word representing this boolean
+           write_addr=None, :PLC register address for write operation
+           write_bit=None,  :Bit to the write in the word for this boolean
+           **kwargs         :
+   
+   kwargs: l=None,       :label of the attribute
+           d=None,       :description of the attribute
+           min=None,     :minimum value allowed
+           max=None,     :maximum value allowed
+           unit=None,    :attribute unit
+           format=None   :In the number case (int/float) its precision
+           events={}     :dictionary where its existence will set up attr events
+                          and its content will configure their behaviour
+           qualities={}  :dictionary where the key represents the available 
+                          qualities and the items the conditions to set them.
+                          Important: alarm > warning > changing
+           formula={}    :dictionary with keys 'read' and 'write' were they 
+                          contain an string that could be introduced in 
+                          an eval() call with the keyword VALUE as the read or
+                          write transformation.
+'''
+
+#def jin(*args):
+#    return john(args)
+
+Attr('Heat_I',
+     PyTango.DevFloat,4,
+     l='Heating current monitor',
+     unit='A',min=0,max=30,format='%4.1f',
+     events={THRESHOLD:0.01},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+Attr('Heat_V',
+     PyTango.DevFloat,8,
+     l='Heating voltage monitor',
+     unit='V',min=0,max=30,format='%4.1f',
+     events={THRESHOLD:0.01},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+Attr('HVPS_V',
+     PyTango.DevFloat,12,#RO
+     l='High voltage PS voltage',
+     unit='kV',min=0,max=40,format='%4.2f',
+     events={THRESHOLD:0.001},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+Attr('HVPS_I',
+     PyTango.DevFloat,16,
+     l='High voltage PS current',
+     unit='mA',min=0,max=150,format='%4.1f',
+     events={THRESHOLD:0.01},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+Attr('Peak_I',
+     PyTango.DevFloat,20,
+     l='Peak current',
+     unit='A',min=0,max=400,format='%4.1f',
+     events={THRESHOLD:0.01},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+Attr('Peak_V',
+     PyTango.DevFloat,24,
+     l='Peak voltage',
+     d='peak voltage (calculated',unit='kV',min=0,max=400,format='%4.1f',
+     events={THRESHOLD:0.01},
+     #qualities={CHANGING:{'rel':0.1}}
+     )
+LV_J = {0:'off',
+        1:'vacuum fault',
+        2:'oil level fault',
+        3:'fan circuit breakers fault',
+        4:'23 C water inlet fault',
+        5:'focusing magnet underflow',
+        6:'focusing magnet overtemp',
+        7:'klystron body underflow',
+        8:'klystron body overtemp',
+        9:'klystron collector underflow',
+        10:'klystron collector overtemp',
+        11:'cooling down (5 minutes)',
+        12:'ready'}
+LV_J_QUALITIES={WARNING:[0,1,2,3,4,5,6,7,8,9,10],
+                CHANGING:[11]}
+Attr('LV_ST',
+     PyTango.DevUChar,37,
+     l='Low voltage status',
+     d='low voltage status'+john(LV_J),
+     meanings=LV_J,
+     qualities=LV_J_QUALITIES,
+     events={})
+F_J = {0:'off',
+       1:'decreasing',
+       2:'low limit fault',
+       3:'high limit fault',
+       4:'heating',
+       5:'ready'}
+F_J_QUALITIES={WARNING:[0,2,3],
+               CHANGING:[1,4]}
+Attr('Heat_ST',
+     PyTango.DevUChar,38,
+     l='Filament heating status',
+     d='filament heating status'+john(F_J),
+     meanings=F_J,
+     qualities=F_J_QUALITIES,
+     events={})
+#modify the documented state meaning string to adapt to user meaning.
+HV_J = {0:'Waiting for Low Voltage',#'heating...',
+        1:'klystron tank covers',
+        2:'PFN earth rod',
+        3:'PFN doors',
+        4:'dumping relay closed',
+        5:'over current',
+        6:'interlock',
+        7:'fault',
+        8:'off',
+        9:'ready'}
+HV_J_QUALITIES={ALARM:[7],
+                WARNING:[0,1,2,3,4,5,6,8]}
+Attr('HVPS_ST',
+     PyTango.DevUChar,39,
+     l='High voltage PS heating status',
+     d='high voltage PS heating status'+john(HV_J),
+     meanings=HV_J,
+     qualities=HV_J_QUALITIES,
+     events={})
+PL_J = {0:'off',
+        1:'focusing B1 undercurrent',
+        2:'focusing B2 undercurrent',
+        3:'focusing B3 undercurrent',
+        4:'DC reset undercurrent',
+        5:'arc overcurrent',
+        6:'RF reflected power',
+        7:'off',
+        8:'ready'}
+PL_J_QUALITIES={WARNING:[0,1,2,3,4,5,6,7]}
+Attr('Pulse_ST',
+     PyTango.DevUChar,40,
+     l='Pulse status',
+     d='pulse status'+john(PL_J),
+     meanings=PL_J,
+     qualities=PL_J_QUALITIES,
+     events={})
+Attr('LV_Time',
+     PyTango.DevShort,42,
+     l='Voltage slow down time',
+     d='tempo stop low voltage (5 min)',unit='s',
+     events={},
+     qualities={CHANGING:{ABSOLUTE:{ABOVE:0,BELOW:300,UNDER:True}}},
+     )
+Attr('Heat_Time',
+     PyTango.DevShort, 44,
+     l='Heating time',
+     d='heating tempo (20 min)',unit='m',
+     events={},
+     #FIXME: even documentation says this is seconds, its clear that it gives 
+     # tenths of seconds. A formula is required to convert to minutes
+     # (integer rounded)
+     formula={'read':'VALUE / 6'},
+     qualities={CHANGING:{ABSOLUTE:{ABOVE:0,BELOW:20,UNDER:True}}},
+     )
+
+#----TODO: This attrbute will 'Rampeable' above some given threashold.
+#          That is from 0 to this threashold is set directly, and above to 
+#          the maximum must follow some steps and some time on each step.
+#          This two ramp parameters must be also dynattrs.
+AttrRampeable('HVPS_V_setpoint',
+     PyTango.DevFloat,46,0,#RW
+     l='High voltage PS voltage setpoint',
+     unit='kV',min=0,max=33,format='%4.2f',
+     events={THRESHOLD:0.005},
+     qualities={CHANGING:{'rel':0.1}},
+     rampsDescriptor = {ASCENDING:{STEP:0.5,#kV
+                                   STEPTIME:1,#s
+                                   THRESHOLD:20,#kV
+                                   SWITCH:'HVPS_ONC'}})
+
+AttrBit('LV_Interlock_RC',
+        62, 0, 16,
+        l='Low voltage reset',
+        d='low voltage reset\nrising edge reset',
+        events={},
+        isRst=True)
+AttrBit('LV_ONC',
+        62, 1, 16,
+        l='Low voltage on',
+        d='low voltage on\nFalse:off\nTrue:on',
+        events={},
+        formula={'read':'VALUE and '\
+                 'self._internalAttrs[\'lv_ready\'][\'read_value\'] == True'},
+        )
+AttrBit('HVPS_Interlock_RC',
+        62, 2, 16,
+        l='High voltage reset',
+        d='high voltage reset\nrising edge reset',
+        events={},
+        isRst=True)
+AttrBit('HVPS_ONC',
+        62 ,3, 16,
+        l='High voltage on',
+        d='high voltage on\nFalse:off\nTrue:on',
+        events={},
+        #rampingAttr='HVPS_V_setpoint',
+        formula={'read':'VALUE and '\
+                       'self._plcAttrs[\'HVPS_ST\'][\'read_value\'] == 9 and '\
+                       'self._plcAttrs[\'Pulse_ST\'][\'read_value\'] == 8',
+                 'write':'VALUE and '\
+                       'self._plcAttrs[\'HVPS_ST\'][\'read_value\'] == 8 and '\
+                       'self._plcAttrs[\'Pulse_ST\'][\'read_value\'] == 7'
+                },
+        switchDescriptor={ATTR2RAMP:'HVPS_V_setpoint',
+                          WHENON:{FROM:'HVPS_V_setpoint_Ascending_Threshold'},
+                          #WHENOFF:{TO:0}#from where the WRITEVALUE say
+                          }
+        )
+
+#AttrPLC(HeartBeat,Lock_ST,rLockingAddr,rLockingBit,wLockingAddr,wLockingBit)
+AttrPLC(36,41,63,0,17,0)
+
+AttrLogic('lv_ready',
+          logic={'LV_ST':[12],
+                 'Heat_ST':[5]},
+          d='Klystron LV ready',
+          l='Klystron LV ready',
+          events={},
+          )
+
+AttrLogic('hvps_ready',
+          logic={'HVPS_ST':[8,9],
+                 'Pulse_ST':[0,7,8]},
+          d='High voltage PS ready',
+          l='High voltage PS ready',
+          events={},
+          )
