e6d538160f07db66e7034910d8f73f39f9176d17
Sergi Blanch-Torne <sblanch@cells.es>
2018-05-15 16:32:29 +0200

    fix meanings property

--- LinacAttrs/meaningAttr.py
+++ LinacAttrs/meaningAttr.py
@@ -55,7 +55,7 @@ class MeaningAttr(_LinacFeature, LinacAttr):
 
     @property
     def meanings(self):
-        return
+        return self._meanings
 
     @meanings.setter
     def meanings(self, value):
