4f7cc2bbe6cceb7f9bbec13ec6010df56cef4998
Sergi Blanch-Torne <sblanch@cells.es>
2018-07-20 16:44:37 +0200

    fix import

--- LinacAttrs/internalAttr.py
+++ LinacAttrs/internalAttr.py
@@ -16,6 +16,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 from .linacAttr import LinacAttr
+from .meaningAttr import MeaningAttr
 
 __author__ = "Lothar Krause and Sergi Blanch-Torne"
 __maintainer__ = "Sergi Blanch-Torne"
